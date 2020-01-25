6d98ce35f1ff63e7df6b3a6239bb1a3482fe983e
lucasbarbosa17 <lucasbarbosa1738@gmail.com>
2020-01-24 21:45:49 -0300

    realizando desafio 1: criação de crud básico

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+const projects = [];
+
+// MIDDLEWARE QUE VERIFICA SE O PROJETO EXISTE
+function checkProjectExists(req, res, next) {
+  let projetoBuscado = projects.find(proj => proj.id == req.params.id);
+
+  if (!projetoBuscado) {
+    return res.status(404).json({ error: 'Project not found.' });
+  }
+
+  req.projetoBuscado = projetoBuscado;
+
+  req.parametrosDoProjetoBuscado = {
+    index: projects.indexOf(projetoBuscado),
+    id: projetoBuscado.id,
+    title: req.body.title
+  };
+
+  return next();
+}
+
+function geradorId() {
+  return projects.length + 1;
+}
+
+//ROTA DE LISTAGEM DOS PROJETOS
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+//ENCONTRA PROJETO PELO ID
+server.get('/projects/:id', checkProjectExists, (req, res) => {
+  // O MIDDLEWARE checkProjectExists já se encarrega de buscar por Id.
+  //  O projeto encontrado "project"
+  // é colocado na requisição atráves da linha req.project = project
+  return res.json(req.projetoBuscado);
+});
+
+//ROTA DE CADASTRO DOS PROJETOS
+server.post('/projects', (req, res) => {
+  const { title } = req.body;
+  projects.push({
+    id: geradorId(),
+    title,
+    tasks: []
+  });
+  return res.json(projects);
+});
+
+// ROTA DE ATUALIZAÇÃO DE PROJETOS
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  req.projetoBuscado.title = req.parametrosDoProjetoBuscado.title;
+  return res.json(projects);
+});
+
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  req.projetoBuscado.tasks.push(req.parametrosDoProjetoBuscado.title);
+  return res.json(projects);
+});
+
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  projects.splice(req.projetoBuscado.index, 1);
+  return res.json(projects);
+});
+
+//CONFIGURAÇÃO DA PORTA DO SERVIDOR
+server.listen(3000);
