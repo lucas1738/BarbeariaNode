6cb169f0475b62f7c4d898f9dc3275bf12b3d95d
lucasbarbosa17 <lucasbarbosa1738@gmail.com>
2020-01-30 20:11:19 -0300

    criando user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,9 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-
+    const user = await User.create(req.body);
+    return res.json();
   }
 }
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 import Router from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '1238712387',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
 
 export default routes;
