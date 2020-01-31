9730abbdf411565c8c5f1ab5ff86c45a3aa8fc96
lucasbarbosa17 <lucasbarbosa1738@gmail.com>
2020-01-30 21:01:40 -0300

    adicionando autenticação jwt ao projeto

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,31 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
-    return res.json();
+    const userExists = await User.findOne({
+      where: {
+
+        email: req.body.email,
+
+
+      },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+    const {
+      id,
+      name,
+      email,
+      provider,
+
+    } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '290d3bfd66b6bb3981aba30b4ad73e6f',
+  expiresIn: '7d',
+};
