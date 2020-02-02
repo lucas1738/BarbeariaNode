fd92a310d716d3789381934974fe05e67266b1fb
lucasbarbosa17 <lucasbarbosa1738@gmail.com>
2020-02-02 20:43:00 -0300

    iniciando desafioDois com a criação da aplicação MeetApp

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "linebreak-style": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+
+
+
+
+  },
+};
